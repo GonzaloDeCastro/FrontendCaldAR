db92368cd0a98f85a2e9aa36d881a5040b5030d3
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-10-26 18:23:00 -0300

    Se corrige titulo unico en tabla de clientes

--- src/Clients/ClientsList.jsx
+++ src/Clients/ClientsList.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import { ClientItem } from './userClient';
+import styles from './style.module.css';
+import {
+  FaTrash as DeleteIcon,
+  FaPen as EditIcon,
+  FaUserTimes as DeleteUser,
+  FaUserEdit
+} from 'react-icons/fa';
 
 export const ClientsList = (props) => {
   const { clientList, onDelete, onEdit } = props;
   return (
     <div>
-      {clientList.map((client) => (
-        <ClientItem
-          key={client.id}
-          clientToShow={client}
-          onDelete={onDelete}
-          onEdit={onEdit}
-        />
-      ))}
+      {' '}
+      <table>
+        <tr>
+          <div className={styles.tittles}>
+            <th scope='col'>Company</th>
+            <th scope='col'>Address</th>
+            <th scope='col'>Phone</th>
+            <th scope='col'>Email</th>
+          </div>
+          <div className={styles.type}>
+            <th scope='col'>Type</th>
+          </div>
+          <div className={styles.boilers}>
+            <td>A</td>
+            <td>B</td>
+            <td>C</td>
+            <td>D</td>
+            <td>
+              <FaUserEdit />
+            </td>
+            <td>
+              <DeleteUser />
+            </td>
+          </div>
+        </tr>
+
+        {clientList.map((client) => (
+          <ClientItem
+            key={client.id}
+            clientToShow={client}
+            onDelete={onDelete}
+            onEdit={onEdit}
+          />
+        ))}
+      </table>
     </div>
   );
 };
