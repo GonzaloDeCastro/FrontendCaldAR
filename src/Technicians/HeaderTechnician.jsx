75843360539b98d124d1b6e3abfde9ef54c3be8f
lucasdelmonte <delmontelucas678@gmail.com>
2021-10-26 17:59:59 -0300

    Se creo el ABM de tecnicos.

---
+++ src/Technicians/HeaderTechnician.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AddTechnician } from './AddTechnician';
+import { EditTechnician} from './EditTechnician';
+import styles from './style.module.css';
+
+export const HeaderTechnician = (props) => {
+  const {
+    showTechnicianForm,
+    setShowTechnicianForm,
+    onAddTechnician,
+    technicianToEdit,
+    onEditTechnician
+  } = props;
+  return (
+    <div className={styles.tittleHeader}>
+      <h1>Technicians</h1>
+      <button
+        className='btn btn-primary'
+        onClick={() =>
+          setShowTechnicianForm({ show: !showTechnicianForm.show, mode: 'Add' })
+        }
+      >
+        {showTechnicianForm.show ? 'Cancel' : 'Add Technician'}
+      </button>
+
+      {showTechnicianForm.show ? (
+        showTechnicianForm.mode === 'Add' ? (
+          <AddTechnician onAddTechnician={onAddTechnician} />
+        ) : (
+          <EditTechnician onEditTechnician={onEditTechnician} technicianToEdit={technicianToEdit} />
+        )
+      ) : undefined}
+    </div>
+  );
+};
--- src/Technicians/index.jsx
+++ src/Technicians/index.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { technicians as initalTechnicians } from '../Mocks/technicians.json';
+import { HeaderTechnician} from './HeaderTechnician';
+import { TechnicianList } from './TechniciansList';
+import { v4 as uuidv4 } from 'uuid';
 
 const Tenchnicians = () => {
+  const [showForm, setShowForm] = useState({ show: false, mode: 'Add' });
+  const [technicians, setTechnicians] = useState(initalTechnicians);
+  const [technicianToEdit, setTechnicianToEdit] = useState(undefined);
 
-  return <div></div>
+  const handleEditClick = (technician) => {
+    setTechnicianToEdit(technician);
+    setShowForm({ show: true, mode: 'Edit' });
+  };
+
+  const handleAddTechnician = (technician) => {
+    technician.id = uuidv4();
+    setTechnicians([...technicians, technician]);
+  };
+
+  const handleDeleteTechnician = (id) => {
+    const newTechnicians = technicians.filter((technician) => technician.id !== id);
+    setTechnicians(newTechnicians);
+  };
+
+  const handleEditTechnician = (technician) => {
+    const newTechnicians = technicians.map((x) => (x.id === technician.id ? technician : x));
+    setTechnicians(newTechnicians);
+  };
+
+  return (
+  <div className='m-3'>
+    <HeaderTechnician
+      showTechnicianForm={showForm}
+      setShowTechnicianForm={setShowForm}
+      onAddTechnician={handleAddTechnician}
+      onEditTechnician={handleEditTechnician}
+      technicianToEdit={technicianToEdit}
+    />
+    <TechnicianList
+      technicianList={technicians}
+      onDelete={handleDeleteTechnician}
+      onEdit={handleEditClick}
+    />
+  </div>
+  );
 };
 
 export default Tenchnicians;
