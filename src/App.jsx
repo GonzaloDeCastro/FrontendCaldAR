d33747721138d300a8c7a0040ed950c3a05c9ce3
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-11-06 18:56:28 -0300

    Primera configuracion en archivo App.jsx para aplicar redux, no está funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'react-redux';
+import { Provider } from 'react-redux';
+//import rootReducer from '.redux/reducers/rootReducer';
 import Routes from './Routes';
+
+const configureStore = () => {
+  const enhancer = composeWithDevTools();
+  return createStore(rootReducer, enhancer);
+};
+
+const store = configureStore();
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 };
